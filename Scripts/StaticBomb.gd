a39e43a34c5d47a963051230d401ab494be33b31
fadop3 <fadop3@web.de>
2022-06-05 12:46:10 +0200

    wall

--- Scripts/StaticBomb.gd
+++ Scripts/StaticBomb.gd
@@ -4,19 +4,19 @@ onready var bomb_explosion = $Explosion
 onready var bomb_radius = $BombRadius_Area2D
 
 
-func ready():
-	# set bomb_explosion sprite visiblity to false
-	bomb_explosion.visible = false
+func ready() -> void:
+    # set bomb_explosion sprite visiblity to false
+    bomb_explosion.visible = false
 
 
-func _on_Timer_timeout():
-	# show bomb explosion and then destory self
-	bomb_explosion.visible = true
-	
-	## check for players in range here and kill them ##
-	for area in bomb_radius.get_overlapping_areas():
-		area.get_parent().queue_free()
-	
-	yield(self.get_tree().create_timer(.5), "timeout")
-	
-	self.queue_free() # self destruct from tree
+func _on_Timer_timeout() -> void:
+    # show bomb explosion and then destory self
+    bomb_explosion.visible = true
+    
+    ## check for players in range here and kill them ##
+    for area in bomb_radius.get_overlapping_areas():
+        area.get_parent().queue_free()
+    
+    yield(self.get_tree().create_timer(.5), "timeout")
+    
+    self.queue_free() # self destruct from tree
